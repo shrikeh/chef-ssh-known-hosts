6ba8eb8a2300f0a8aa3a74784f2799aca3561e36
shrikeh <shrikeh@gmail.com>
2016-07-04 10:47:10 +0100

    Fix tests

--- test/integration/default/cucumber/features/step_definitions/user_known_host_existing_steps.rb
+++ test/integration/default/cucumber/features/step_definitions/user_known_host_existing_steps.rb
@@ -11,7 +11,7 @@ Given(/^their known hosts file is "([^"]*)"$/) do |host_file|
   `touch #{@host_file}`
 end
 
-Given(/there are existing entries/) do
+Given(/^there are existing entries$/) do
   existing_entries = <<-EOT
 github.com,192.30.252.128 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
 EOT
@@ -19,7 +19,9 @@ EOT
   `#{cmd}`
 end
 
-When(/^the node has new known host entries$/) do
+When(/^Chef runs and the node has new known host entries$/) do
+  @chef_run = `cd /tmp/kitchen/ && sudo chef-solo -j dna.json -c solo.rb`
+  @success = $?.to_i
 end
 
 Then(/^new entries are appended and the existing entries are preserved$/) do
--- test/integration/default/cucumber/features/sysadmin/user_known_host_existing.feature
+++ test/integration/default/cucumber/features/sysadmin/user_known_host_existing.feature
@@ -5,5 +5,5 @@ Scenario: deploy user has existing entries in their ssh known_hosts file
 Given the user account is "deploy"
 And their known hosts file is "/tmp/foo"
 And there are existing entries
-When the node has new known host entries
+When Chef runs and the node has new known host entries
 Then new entries are appended and the existing entries are preserved
