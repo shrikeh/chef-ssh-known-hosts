ae4c0d5d130f04e72c6541dca2f9501385d8b74a
shrikeh <shrikeh@gmail.com>
2016-07-04 17:42:09 +0100

    Rubocop fix for matcher

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,9 @@
 if defined?(ChefSpec)
-  def create_ssh_known_hosts_entry(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:ssh_known_hosts_entry, :create, resource_name)
+  def create_ssh_user_known_hosts_entry(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :ssh_known_hosts_entry,
+      :create,
+      resource_name
+    )
   end
 end
